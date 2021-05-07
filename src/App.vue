5b8c1b5f356020fe5dc862790b1b70c8e7526e38
杨志清 <a424021424@gmail.com>
2021-05-07 15:45:16 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <!--<img alt="Vue logo" src="./assets/logo.png" />-->
+  app vue
+
+  变量名转换
+
+  <!--<HelloWorld msg='Hello Vue 3 + TypeScript + Vite'/>-->
+  <VarNameConvert></VarNameConvert>
+</template>
+
+<script lang='ts'>
+import {defineComponent} from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import VarNameConvert from './views/var-name-convert.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    VarNameConvert
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/var-name-convert.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class='var-name-convert'>
+
+      <p>msg: {{msg}}</p>
+      <div class='checkbox-item'>
+        <span>转换中划线: </span>
+        <input type='checkbox' v-model='transMiddleDash'>
+      </div>
+      <div class='checkbox-item'>
+        <span>转换空格: </span>
+        <input type='checkbox' v-model='transSpace'>
+      </div>
+      <hr>
+      <p>驼峰: {{underlineToHump(msg)}}</p>
+      <p>下划线: {{humpToUnderline(msg)}}</p>
+      <p>大写下划线: {{toCapitalUnderline(msg)}}</p>
+      <div class='child'>
+        <h4>需要转换的变量名</h4>
+        <input type='text' v-model='msg2' @input='handleInput'>
+        <!--<custom-input v-model:mv='msg' :transMiddleDash='transMiddleDash'></custom-input>-->
+        <!-- <custom-input :model-value="msg" @update:model-value="msg = $event"></custom-input> -->
+      </div>
+
+  </div>
+</template>
+
+<script>
+/**
+ *  Created by en20 on 2021/5/7.
+ *  Description:
+ */
+export default {
+  name: "VarNameConvert",
+  props: {},
+  data() {
+    return {
+      msg: 'fooBar',
+      count: 0,
+      transMiddleDash: true,
+      transSpace: true,
+    }
+  },
+  computed: {
+    msg2: {
+      get() {
+        return this.msg
+      },
+      set(v) {
+        let t = v
+        if (this.transSpace) {
+          t = v.replaceAll(' ', '_')
+        }
+        if (this.transMiddleDash) {
+          t = t.replaceAll('-', '_')
+        }
+        this.msg = t
+        // this.$emit('update:mv', v.replaceAll(' ', '_'))
+      },
+    },
+  },
+  watch: {
+    transMiddleDash() {
+      console.log(1111111)
+    },
+  },
+  methods: {
+    handleInput(v) {
+      console.log('handleInput', v.target.value)
+    },
+    // 下划线转驼峰
+    underlineToHump(s) {
+      var a = s.split("_")
+      var result = a[0]
+      for (var i = 1; i < a.length; i++) {
+        result = result + a[i].slice(0, 1).toUpperCase() + a[i].slice(1)
+      }
+      return result
+    },
+    // 驼峰转下划线
+    humpToUnderline(str) {
+      return str.replace(/([A-Z])/g, "_$1").toLowerCase()
+    },
+    // 大写
+    toCapitalUnderline(str) {
+      return str.replace(/([A-Z])/g, "_$1").toUpperCase()
+    },
+  },
+}
+</script>
+
+<style scoped  >
+.var-name-convert {
+
+}
+</style>
